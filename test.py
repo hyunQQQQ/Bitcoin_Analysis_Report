19826ab47305b8c5a8584378280bf844dee6e065
최현규 <hyunq9504@gmail.com>
2025-05-26 16:39:42 +0900

    안정성 추가

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# 이 경로는 반드시 루트 기준
-load_dotenv(dotenv_path=".env")
-
-print("✅ TEST - NAVER_CLIENT_ID:", os.getenv("NAVER_CLIENT_ID"))
-print("✅ TEST - NAVER_CLIENT_SECRET:", os.getenv("NAVER_CLIENT_SECRET"))
